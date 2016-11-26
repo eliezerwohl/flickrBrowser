2e376c087554b9cb5e0890475530bcae4e26ec51
Eliezer Wohl <eliezerwohl@gmail.com>
2016-11-26 15:26:45 -0500

    getflickrjsondata

---
+++ app/src/main/java/com/eliezerwohl/flickrbrowser/GetFlickrJsonData.java
@@ -0,0 +1,106 @@
+package com.eliezerwohl.flickrbrowser;
+
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.tag;
+
+/**
+ * Created by Elie on 11/26/2016.
+ */
+
+class GetFlickrJsonData implements GetRawData.OnDownloadComplete {
+    private static final String TAG = "GET flickr json data";
+
+    private List<Photo> mPhotoList =null;
+    private String mBaseURL;
+    private String mLanguage;
+    private boolean mMatchAll;
+
+    private final OnDataAvailable mCallBack;
+    interface OnDataAvailable {
+        void onDataAvailable(List<Photo> data, DownloadStatus status);
+    }
+
+    public GetFlickrJsonData(OnDataAvailable callBack, String baseURL, String language, boolean matchAll) {
+        Log.d(TAG, "Get flckir json called");
+        mBaseURL = baseURL;
+        mCallBack = callBack;
+        mLanguage = language;
+        mMatchAll = matchAll;
+    }
+    void executeOnSameThreat(String searchCriteria){
+        Log.d(TAG, "executeOnSameThreat: starts");
+        String destinationUri = createUri (searchCriteria, mLanguage, mMatchAll);
+
+        GetRawData getRawData = new GetRawData(this);
+        getRawData.execute(destinationUri);
+        Log.d(TAG, "executeOnSameThreat: ends");
+    }
+
+    @Override
+    public void onDownloadComplete(String data, DownloadStatus status) {
+        Log.d(TAG, "onDownloadComplete: staqrt.  status = " + status);
+        if (status == DownloadStatus.OK) {
+            mPhotoList = new ArrayList<>();
+
+            try {
+                JSONObject jsonData = new JSONObject(data);
+                JSONArray itemsArray = jsonData.getJSONArray("items");
+
+                for (int i = 0; i < itemsArray.length(); i++) {
+                    JSONObject jsonPhoto = itemsArray.getJSONObject(i);
+                    String title = jsonPhoto.getString("title");
+                    String author = jsonPhoto.getString("author");
+                    String author_id = jsonPhoto.getString("author_id");
+                    String tags = jsonPhoto.getString("tags");
+
+                    JSONObject jsonMedia = jsonPhoto.getJSONObject("media");
+                    String photoUrl = jsonMedia.getString("m");
+
+                    String link = photoUrl.replaceFirst("_m.", "_b.");
+                    Photo photoObject = new Photo(title, author, author_id, link, tags, photoUrl);
+                    mPhotoList.add(photoObject);
+
+                    Log.d(TAG, "onDownloadComplete: " + photoObject.toString());
+                }
+            } catch (JSONException jsone) {
+                jsone.printStackTrace();
+                Log.e(TAG, "onDownloadComplete: there's an error joson data" + jsone.getMessage());
+                status = DownloadStatus.FAILED_OR_EMPTY;
+            }
+        }
+        if (mCallBack != null){
+            mCallBack.onDataAvailable(mPhotoList, status);
+        }
+    }
+
+    private String createUri(String searchCriteria, String lang, boolean matchAll){
+        Log.d(TAG, "createUri: starts");
+        Uri uri = Uri.parse(mBaseURL);
+        Uri.Builder builder = uri.buildUpon();
+        builder = builder.appendQueryParameter("tags", searchCriteria);
+        builder = builder.appendQueryParameter("tagmode", matchAll ? "ALL" : "ANY");
+        builder = builder.appendQueryParameter("long", lang);
+        builder = builder.appendQueryParameter("format", "json");
+        builder = builder.appendQueryParameter("nojsoncallback", "1");
+
+        return Uri.parse(mBaseURL).buildUpon()
+                .appendQueryParameter("tags", searchCriteria)
+                .appendQueryParameter("tagmode", matchAll ? "All": "ANY")
+                .appendQueryParameter("lang", lang)
+                .appendQueryParameter("format", "json")
+                .appendQueryParameter("nojsoncallback", "1")
+                .build().toString();
+
+
+    }
+
+}
